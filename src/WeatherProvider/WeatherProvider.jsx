6e3089da4d657f817aba5ec75aaa736e16e69bed
jyc-coder <jyc4648@gmail.com>
2022-09-30 09:45:05 +0900

    #2 - openWeather API를 이용한 상태관리 개발
    openWeather API 에서 `ONECALL`API 와 `Current Weather Data API`를 사용해서  데이터 값을 받아오는지 동작 확인
    
    * `ONECALL` api에서 `hourly` 데이터 객체를 받아오고, 나머지 모든 데이터는 `Current Weather Data API`에서 가져옴
    
    
    * API KEY는 env를 따로 생성하여 숨김

---
+++ src/WeatherProvider/WeatherProvider.jsx
@@ -0,0 +1,55 @@
+import React, { createContext, useEffect, useState } from 'react';
+
+export const WeatherContext = createContext({});
+
+const WeatherProvider = ({ children }) => {
+  const WeatherapiKey = process.env.REACT_APP_WEATHER_API_KEY;
+  const [weatherInfo, setWeatherInfo] = useState({});
+
+  const getWeatherInfo = async () => {
+    try {
+      const currentWeatherInfoAPI = `https://api.openweathermap.org/data/2.5/weather?appid=${WeatherapiKey}&q=seoul&units=metric`;
+      const currentWeatherInfo = await fetch(currentWeatherInfoAPI);
+      const {
+        name,
+        coord: { lat, lon },
+        main: { temp, humidity, pressure, feels_like, temp_min, temp_max },
+        sys: { sunset, sunrise },
+        weather: [{ main: weatherState }],
+        wind: { speed, deg },
+      } = await currentWeatherInfo.json();
+
+      const hourlyWeatherInfoAPI = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=current,daily&appid=${WeatherapiKey}&units=metric`;
+      const hourlyWeatherInfo = await fetch(hourlyWeatherInfoAPI);
+      const { hourly } = await hourlyWeatherInfo.json();
+      setWeatherInfo({
+        name,
+        temp,
+        humidity,
+        pressure,
+        weatherState,
+        speed,
+        deg,
+        hourly,
+        sunset,
+        sunrise,
+        temp_max,
+        temp_min,
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  useEffect(() => {
+    getWeatherInfo();
+  }, []);
+  return (
+    <div>
+      <WeatherContext.Provider value={{ ...weatherInfo }}>
+        {children}
+      </WeatherContext.Provider>
+    </div>
+  );
+};
+
+export default WeatherProvider;
